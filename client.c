ed162faf9ba96009a351c97b1a60887478d72fdc
jordanSu <newjordansu1126@gmail.com>
2015-10-12 00:45:36 +0800

    let client connect

--- client.c
+++ client.c
@@ -5,18 +5,26 @@
 #include <netdb.h>  //struct hostent
 #include <netinet/in.h>  //struct sockaddr_in
 
+#define CONNECT_ERROR -1
+
 #define PORT_NO 5678
 
-int main(int args, char* argv[]) {
+void perr(char* message);
+
+void main(int args, char* argv[]) {
     int socketfd;
     struct sockaddr_in serv_addr;
     struct hostent* server_info;
 
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (&socketfd == NULL) {
+        perr("Socket build error!");
+    }
+
     // analyze the hostname to find address
     server_info = gethostbyname(argv[1]);
     if (server_info == NULL) {
-        printf("Host not found!");
-        exit(1);
+        perr("Host not found!");
     }
 
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -24,5 +32,15 @@ int main(int args, char* argv[]) {
     memcpy(&serv_addr.sin_addr.s_addr, server_info->h_addr, sizeof(server_info->h_addr));
     printf ("this is address:%d",serv_addr.sin_addr.s_addr);
     serv_addr.sin_port = htons(PORT_NO);
-    return 0;
+
+    if (connect(socketfd,(struct sockaddr*) &serv_addr, sizeof(serv_addr)) == CONNECT_ERROR) {
+        perr("Host not found!");
+    }
+
+    
+}
+
+void perr(char* message) {
+    printf("%s", message);
+    exit(1);
 }
